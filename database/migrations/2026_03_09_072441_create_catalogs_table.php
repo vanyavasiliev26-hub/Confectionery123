d610fefa81e7982c9208cc63582cd3bf10bc63cc
vanyavasiliev26-hub <vanya.vasiliev.26@gmail.com>
2026-03-11 04:06:33 +0400

    Add files via upload

---
+++ database/migrations/2026_03_09_072441_create_catalogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatalogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalogs', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text('description');
+            $table->string('image');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogs');
+    }
+}
